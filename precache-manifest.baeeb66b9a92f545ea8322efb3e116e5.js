061efbea39df7a5e96dc8a219b0ddcabf4d5c80a
wolv66 <wolv66@users.noreply.github.com>
2020-04-08 15:40:31 -0700

    Updates

---
+++ precache-manifest.baeeb66b9a92f545ea8322efb3e116e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aca8d04d2e0d8bc4687dcfb2d7e94423",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "cbba59e9ec9a234263a5",
+    "url": "/Todo-App/static/css/2.bb342caa.chunk.css"
+  },
+  {
+    "revision": "7b70b0c03afd47061f5e",
+    "url": "/Todo-App/static/css/main.9eab46d4.chunk.css"
+  },
+  {
+    "revision": "cbba59e9ec9a234263a5",
+    "url": "/Todo-App/static/js/2.77d16f50.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Todo-App/static/js/2.77d16f50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b70b0c03afd47061f5e",
+    "url": "/Todo-App/static/js/main.85b50aa7.chunk.js"
+  },
+  {
+    "revision": "06461d7aa2b1fe196fed",
+    "url": "/Todo-App/static/js/runtime-main.39b0be61.js"
+  }
+]);
