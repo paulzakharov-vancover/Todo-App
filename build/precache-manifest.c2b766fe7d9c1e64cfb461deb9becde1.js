735dfbdfb4f11edf9cf202c53f901b971653158d
wolv66 <wolv66@users.noreply.github.com>
2020-04-08 20:49:41 -0700

    made more responsive

---
+++ build/precache-manifest.c2b766fe7d9c1e64cfb461deb9becde1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27685a67b79317f46ef8354b7182b2cc",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "799e9b20432326719e3a",
+    "url": "/Todo-App/static/css/2.bb342caa.chunk.css"
+  },
+  {
+    "revision": "75d5f3365b30df607e93",
+    "url": "/Todo-App/static/css/main.d64e57d1.chunk.css"
+  },
+  {
+    "revision": "799e9b20432326719e3a",
+    "url": "/Todo-App/static/js/2.5c4098c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Todo-App/static/js/2.5c4098c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75d5f3365b30df607e93",
+    "url": "/Todo-App/static/js/main.ff121835.chunk.js"
+  },
+  {
+    "revision": "06461d7aa2b1fe196fed",
+    "url": "/Todo-App/static/js/runtime-main.39b0be61.js"
+  }
+]);
