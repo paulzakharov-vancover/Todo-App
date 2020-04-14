248d05263e00acc36df1171f297349a707d9f3dc
Wolv66 <wolv66@gmail.com>
2020-04-13 17:34:39 -0700

    Updates

--- precache-manifest.031e9070a051820aca1433fa6f25d535.js
+++ precache-manifest.031e9070a051820aca1433fa6f25d535.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a3498890428c670668c2612f14ccc110",
+    "revision": "78832ae3b23d566e4fd8fd4545a1dd70",
     "url": "/Todo-App/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Todo-App/static/css/2.bb342caa.chunk.css"
   },
   {
-    "revision": "8b0b9fd86eb60373d59c",
-    "url": "/Todo-App/static/css/main.133cf95c.chunk.css"
+    "revision": "55c7c9eaebad4509531a",
+    "url": "/Todo-App/static/css/main.262b9d9e.chunk.css"
   },
   {
     "revision": "799e9b20432326719e3a",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Todo-App/static/js/2.5c4098c1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8b0b9fd86eb60373d59c",
-    "url": "/Todo-App/static/js/main.ff121835.chunk.js"
+    "revision": "55c7c9eaebad4509531a",
+    "url": "/Todo-App/static/js/main.33a039c1.chunk.js"
   },
   {
     "revision": "06461d7aa2b1fe196fed",
